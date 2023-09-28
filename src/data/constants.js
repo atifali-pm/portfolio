cf566ed0f95622ef2950ae86b57af6a6d35062d8
Atif Ali <atif@monotote.com>
2023-09-29 02:02:55 +0500

    WIP

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,11 +10,11 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/atifali-pm",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
-  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-  twitter: "https://twitter.com/RishavChanda",
-  insta: "https://www.instagram.com/rishav_chanda/",
-  facebook: "https://www.facebook.com/rishav.chanda.165/",
+    "https://drive.google.com/file/d/1AKLA7V77TEmyJT4I87JehTAV77eFEx8y/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/-atifali-/",
+  twitter: "https://twitter.com/aatifali",
+  insta: "https://www.instagram.com/hello_atif/",
+  facebook: "https://www.facebook.com/hello.atif/",
 };
 
 export const skills = [
